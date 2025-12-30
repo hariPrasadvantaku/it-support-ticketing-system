f0c822695ee7a969e645d00a62ab344e36f209ec
Hari Prasad <hariprasadvantaku@gmail.com>
2025-12-30 13:20:02 +0530

    configure spring security filter chain and authentication manager

---
+++ ticketing/src/main/java/com/itsupport/ticketing/config/SecurityConfig.java
@@ -0,0 +1,80 @@
+package com.itsupport.ticketing.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.itsupport.ticketing.security.CustomLoginSuccessHandler;
+import com.itsupport.ticketing.security.CustomUserDetailsService;
+
+@Configuration
+public class SecurityConfig {
+	
+	 private final CustomUserDetailsService userDetailsService;
+	    private final CustomLoginSuccessHandler successHandler;
+
+	    public SecurityConfig(CustomUserDetailsService userDetailsService,
+	                          CustomLoginSuccessHandler successHandler) {
+	        this.userDetailsService = userDetailsService;
+	        this.successHandler = successHandler;
+	    }
+
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/login","/signup","/css/**").permitAll()
+					.requestMatchers("/admin/**").hasRole("ADMIN")
+					.requestMatchers("/support/**").hasRole("SUPPORT")
+	                .requestMatchers("/user/**").hasRole("USER")
+	                .anyRequest().authenticated()
+			)
+			
+			.formLogin(form -> form
+				    .loginPage("/login")
+				    .loginProcessingUrl("/login")   
+				    .successHandler(successHandler)
+				    .permitAll()
+				)
+
+
+			.logout(logout -> logout
+		                .logoutUrl("/logout")
+		                .logoutSuccessUrl("/login")
+		    )
+			.exceptionHandling(ex -> ex
+				    .accessDeniedPage("/access-denied")
+			);
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(
+	        HttpSecurity http,
+	        PasswordEncoder passwordEncoder) throws Exception {
+
+	    AuthenticationManagerBuilder builder =
+	            http.getSharedObject(AuthenticationManagerBuilder.class);
+
+	    builder
+	        .userDetailsService(userDetailsService)
+	        .passwordEncoder(passwordEncoder);
+
+	    return builder.build();
+	}
+
+}
