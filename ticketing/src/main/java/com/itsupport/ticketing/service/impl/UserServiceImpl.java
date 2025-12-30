26dfd874aa50a766b0b3b1a6abad6d7ac8b50a46
Hari Prasad <hariprasadvantaku@gmail.com>
2025-12-30 13:24:36 +0530

    encrypt user passwords using bcrypt

--- ticketing/src/main/java/com/itsupport/ticketing/service/impl/UserServiceImpl.java
+++ ticketing/src/main/java/com/itsupport/ticketing/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.itsupport.ticketing.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.itsupport.ticketing.entity.User;
@@ -13,13 +14,18 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	
 	@Override
 	public User registerUser(User user) {
 		User exitingUser = userRepository.findByEmail(user.getEmail());
 		if(exitingUser != null) {
 			throw new RuntimeException("Email already registered");
 		}
-		user.setRole("USER");
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+	    user.setRole("ROLE_USER");
 		userRepository.save(user);
 		return null;
 	}
