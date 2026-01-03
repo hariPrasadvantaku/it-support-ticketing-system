cb9b80e37f9f6c0e112dabcc3dbb0c110d5455d4
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-03 12:58:41 +0530

    add TicketServiceImpl

---
+++ ticketing/src/main/java/com/itsupport/ticketing/service/impl/TicketServiceImpl.java
@@ -0,0 +1,38 @@
+package com.itsupport.ticketing.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.itsupport.ticketing.entity.Ticket;
+import com.itsupport.ticketing.entity.TicketStatus;
+import com.itsupport.ticketing.entity.User;
+import com.itsupport.ticketing.repository.TicketRepository;
+import com.itsupport.ticketing.service.TicketService;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+	private final TicketRepository ticketRepository ;
+	
+	public TicketServiceImpl(TicketRepository ticketRepository) {
+		this.ticketRepository = ticketRepository;
+	}
+
+	@Override
+	public void createTicket(Ticket ticket, User user) {
+		// TODO Auto-generated method stub
+		ticket.setUser(user);
+		ticket.setStatus(TicketStatus.OPEN);
+		ticket.setCreatedAt(LocalDateTime.now());
+		ticketRepository.save(ticket);
+	}
+
+	@Override
+	public List<Ticket> getTicketForUser(User user) {
+		// TODO Auto-generated method stub
+		
+		return ticketRepository.findByUser(user);
+	}
+	
+}
