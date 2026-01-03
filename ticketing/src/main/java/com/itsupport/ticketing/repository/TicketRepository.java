e7ed11b08e7af79da5c9cb37f64be13e4746ff01
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-03 12:39:56 +0530

    add findByUser method

--- ticketing/src/main/java/com/itsupport/ticketing/repository/TicketRepository.java
+++ ticketing/src/main/java/com/itsupport/ticketing/repository/TicketRepository.java
@@ -1,5 +1,12 @@
 package com.itsupport.ticketing.repository;
 
-public interface TicketRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itsupport.ticketing.entity.Ticket;
+import com.itsupport.ticketing.entity.User;
+
+public interface TicketRepository extends JpaRepository {
+	List<Ticket> findByUser(User user);
 }
