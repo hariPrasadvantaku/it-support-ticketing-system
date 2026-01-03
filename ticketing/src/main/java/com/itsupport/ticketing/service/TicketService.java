680e885ed3150ebd27df46b9516d9c8e64935ea3
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-03 12:56:35 +0530

    add methods

--- ticketing/src/main/java/com/itsupport/ticketing/service/TicketService.java
+++ ticketing/src/main/java/com/itsupport/ticketing/service/TicketService.java
@@ -1,5 +1,11 @@
 package com.itsupport.ticketing.service;
 
-public interface TicketService {
+import java.util.List;
 
+import com.itsupport.ticketing.entity.Ticket;
+import com.itsupport.ticketing.entity.User;
+
+public interface TicketService {
+	void createTicket(Ticket ticket,User user);
+	List<Ticket> getTicketForUser(User user);
 }
