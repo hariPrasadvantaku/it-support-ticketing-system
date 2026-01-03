98c34d5f799e68f6e07dfaa580f30fe5605fb7b5
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-03 12:33:25 +0530

    remove the status

--- ticketing/src/main/java/com/itsupport/ticketing/entity/User.java
+++ ticketing/src/main/java/com/itsupport/ticketing/entity/User.java
@@ -5,13 +5,9 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -34,12 +30,7 @@ public class User {
     private String password;
 
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private TicketStatus status; 
+    private String role; 
     
     private LocalDateTime createdAt;
-    
-    @ManyToOne
-    @JoinColumn(name="user_id")
-    private User user;
 }
