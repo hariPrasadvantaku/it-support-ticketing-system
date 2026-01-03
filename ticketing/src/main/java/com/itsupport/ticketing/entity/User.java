b789daa23b6ee09e81e1edc4fa19640dcc2b0bf0
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-03 12:27:22 +0530

    add LocalDateTime and Status

--- ticketing/src/main/java/com/itsupport/ticketing/entity/User.java
+++ ticketing/src/main/java/com/itsupport/ticketing/entity/User.java
@@ -1,7 +1,18 @@
 package com.itsupport.ticketing.entity;
 
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
@@ -23,5 +34,12 @@ public class User {
     private String password;
 
     @Column(nullable = false)
-    private String role; 
+    @Enumerated(EnumType.STRING)
+    private TicketStatus status; 
+    
+    private LocalDateTime createdAt;
+    
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
 }
