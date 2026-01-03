63f5cc96eb068a46f7bf22a6c984d499da973b7f
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-03 12:38:55 +0530

    add Ticket Class

---
+++ ticketing/src/main/java/com/itsupport/ticketing/entity/Ticket.java
@@ -0,0 +1,44 @@
+package com.itsupport.ticketing.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String title;
+
+    @Column(length = 1000,nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TicketStatus status;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
