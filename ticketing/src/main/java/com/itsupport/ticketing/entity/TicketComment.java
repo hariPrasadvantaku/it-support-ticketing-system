6d3e68f58d3e180c261ac25c30b8f0a76f659f64
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-05 22:47:59 +0530

    add ticket comment entity for history tracking

---
+++ ticketing/src/main/java/com/itsupport/ticketing/entity/TicketComment.java
@@ -0,0 +1,31 @@
+package com.itsupport.ticketing.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 1000, nullable = false)
+    private String comment;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
