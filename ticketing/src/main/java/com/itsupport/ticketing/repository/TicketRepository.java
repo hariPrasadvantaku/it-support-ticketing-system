6f109a268ad436da6199d887cbf7640ee613bb29
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-05 12:23:37 +0530

    add findByStatus method in Ticket Repo

--- ticketing/src/main/java/com/itsupport/ticketing/repository/TicketRepository.java
+++ ticketing/src/main/java/com/itsupport/ticketing/repository/TicketRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.itsupport.ticketing.entity.Ticket;
+import com.itsupport.ticketing.entity.TicketStatus;
 import com.itsupport.ticketing.entity.User;
 
 public interface TicketRepository extends JpaRepository<Ticket,Long> {
 	List<Ticket> findByUser(User user);
+	List<Ticket> findByStatus(TicketStatus status);
 }
