dc98b12860b312b2236938844502158f255929f4
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-05 12:25:40 +0530

    add methods getTicketsForSupport and updateTicketStatus

--- ticketing/src/main/java/com/itsupport/ticketing/service/TicketService.java
+++ ticketing/src/main/java/com/itsupport/ticketing/service/TicketService.java
@@ -3,9 +3,12 @@ package com.itsupport.ticketing.service;
 import java.util.List;
 
 import com.itsupport.ticketing.entity.Ticket;
+import com.itsupport.ticketing.entity.TicketStatus;
 import com.itsupport.ticketing.entity.User;
 
 public interface TicketService {
 	void createTicket(Ticket ticket,User user);
 	List<Ticket> getTicketsForUser(User user);
+	List<Ticket> getTicketsForSupport();
+    void updateTicketStatus(Long ticketId, TicketStatus status);
 }
