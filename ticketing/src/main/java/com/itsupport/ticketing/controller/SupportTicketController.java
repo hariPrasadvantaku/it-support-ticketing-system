bd071b85c1d6a3bff94240871cdf65d6af9eabd6
Hari Prasad <hariprasadvantaku@gmail.com>
2026-01-05 22:14:50 +0530

    restrict support users to view only assigned tickets

--- ticketing/src/main/java/com/itsupport/ticketing/controller/SupportTicketController.java
+++ ticketing/src/main/java/com/itsupport/ticketing/controller/SupportTicketController.java
@@ -2,12 +2,16 @@ package com.itsupport.ticketing.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.itsupport.ticketing.entity.Ticket;
 import com.itsupport.ticketing.entity.TicketStatus;
+import com.itsupport.ticketing.entity.User;
+import com.itsupport.ticketing.repository.UserRepository;
 import com.itsupport.ticketing.service.TicketService;
 
 @Controller
@@ -15,18 +19,29 @@ import com.itsupport.ticketing.service.TicketService;
 public class SupportTicketController {
 
     private final TicketService ticketService;
+    private final UserRepository userRepository;
 
-    public SupportTicketController(TicketService ticketService) {
+    public SupportTicketController(TicketService ticketService,UserRepository userRepository) {
         this.ticketService = ticketService;
+		this.userRepository = userRepository;
     }
 
     
     @GetMapping
     public String viewTickets(Model model) {
-        List<Ticket> tickets = ticketService.getTicketsForSupport();
-        model.addAttribute("tickets", tickets);
-        model.addAttribute("statuses", TicketStatus.values());
-        return "support/tickets";
+    	Authentication auth =
+    	        SecurityContextHolder.getContext().getAuthentication();
+
+    	    User supportUser =
+    	        userRepository.findByEmail(auth.getName());
+
+    	    List<Ticket> tickets =
+    	        ticketService.getTicketsForSupport(supportUser);
+
+    	    model.addAttribute("tickets", tickets);
+    	    model.addAttribute("statuses", TicketStatus.values());
+
+    	    return "support/tickets";
     }
 
    
