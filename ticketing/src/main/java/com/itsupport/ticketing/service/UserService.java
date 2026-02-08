50e779717e3392bbe7050752abd55cbfd788d7d6
Hari Prasad <hariprasadvantaku@gmail.com>
2026-02-08 13:20:49 +0530

    Update UserService with password update support

--- ticketing/src/main/java/com/itsupport/ticketing/service/UserService.java
+++ ticketing/src/main/java/com/itsupport/ticketing/service/UserService.java
@@ -5,4 +5,5 @@ import com.itsupport.ticketing.entity.User;
 public interface UserService {
 	User registerUser(User user);
 	User login(String email,String password);
+	public void updateUser(User updatedUser,String newPassword);
 }
