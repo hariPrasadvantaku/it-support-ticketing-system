19ba944b3f49ef87a9d155072d95c8ce890a3454
Hari Prasad <hariprasadvantaku@gmail.com>
2026-02-08 13:20:24 +0530

    Update TicketRepository queries for user ticket handling

--- ticketing/src/main/java/com/itsupport/ticketing/repository/TicketRepository.java
+++ ticketing/src/main/java/com/itsupport/ticketing/repository/TicketRepository.java
@@ -12,4 +12,7 @@ public interface TicketRepository extends JpaRepository<Ticket,Long> {
 	List<Ticket> findByUser(User user);
 	List<Ticket> findByStatus(TicketStatus status);
 	List<Ticket> findByAssignedTo(User user);
+	void deleteByUser(User user);
+	void deleteByAssignedTo(User user);
+
 }
